071defc4299ec4928d333fd49d82b926c6a214ed
alicez <jyz2003@cs.washington.edu>
2022-12-14 22:40:31 -0800

    [new] created react app and added 'coming soon' unstylized text

---
+++ tedx-website/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ComingSoon from './ComingSoon.js'
+
+function App() {
+    return (
+        <div className="App">
+            <ComingSoon />
+        </div>
+    );
+}
+
+export default App;
---
+++ tedx-website/src/ComingSoon.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ComingSoon(props) {
+    return (
+        <div className="content-section">
+            <h2>COMING SOON</h2>
+        </div>
+    );
+}
+
+export default ComingSoon;
