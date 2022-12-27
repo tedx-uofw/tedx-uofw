3f7ebead4e5c88b4576167d9870cfdcca28c7bfc
alicez <jyz2003@cs.washington.edu>
2022-12-27 10:32:28 -0800

    [update] import nue haas display font and update Coming Soon text

--- tedx-website/src/components/ComingSoon.js
+++ tedx-website/src/components/ComingSoon.js
@@ -5,7 +5,7 @@ function ComingSoon(props) {
     return (
         <div className="content-section">
             <div className="main-header">
-                <h2>COMING SOON</h2>
+                <h2>Coming Soon</h2>
             </div>
         </div>
     );
