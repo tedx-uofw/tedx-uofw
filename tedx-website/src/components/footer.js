76145cb62edf68770d7ed5367a744c864168c38a
alicez <jyz2003@cs.washington.edu>
2023-01-07 13:08:50 -0800

    [updates] increase margin and left-align footer

--- tedx-website/src/components/footer.js
+++ tedx-website/src/components/footer.js
@@ -9,25 +9,22 @@ import mailicon from '../assets/mail.svg';
 
 function Footer(props){
     return (
-        <div>
-            <div id="root"> </div>
-            <div id="footer">
-                <p id = "leftContent">© TEDxUofW 2022 | SEEING THE INVISIBLE</p>
-                <div id= "rightContent">
-                    <a className ="icons" href="mailto:tedxuofw@uw.edu">
-                        <img className="img" src={mailicon} alt=""></img>
-                    </a>
-                    <a className ="icons" href= "https://www.facebook.com/TEDxUofW/">
-                        <img className="img" src={facebooklogo} alt=""></img>
-                    </a>
-                    <a className ="icons" href="https://www.instagram.com/tedxuofw/?hl=en">
-                        <img className="img" src={instagramlogo} alt=""></img>
-                    </a>
-                    <a  className ="icons" href="https://www.linkedin.com/company/tedxuofw/about/">
-                        <img className="img" src={linkedinlogo} alt=""></img>
-                    </a>
-                </div>
-            </div>
+        <div id="footer">
+            <p id = "leftContent">© TEDxUofW 2022 | SEEING THE INVISIBLE</p>
+            <div id= "rightContent">
+                <a className ="icons" href="mailto:tedxuofw@uw.edu">
+                    <img className="img" src={mailicon} alt=""></img>
+                </a>
+                <a className ="icons" href= "https://www.facebook.com/TEDxUofW/">
+                    <img className="img" src={facebooklogo} alt=""></img>
+                </a>
+                <a className ="icons" href="https://www.instagram.com/tedxuofw/?hl=en">
+                    <img className="img" src={instagramlogo} alt=""></img>
+                </a>
+                <a  className ="icons" href="https://www.linkedin.com/company/tedxuofw/about/">
+                    <img className="img" src={linkedinlogo} alt=""></img>
+                </a>
+             </div>
         </div>
     )
 }
