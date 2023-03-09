003aefc2f116b957786aac45357d432d4329510c
ruhee uw <ruheer@uw.edu>
2023-03-08 21:02:38 -0800

    fixed alignment with title and overlap issue

--- tedx-website/src/components/about_tedx/about_tedx.js
+++ tedx-website/src/components/about_tedx/about_tedx.js
@@ -6,26 +6,26 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+
 function AboutTedX(props) {
 
 
     return (
         <div className="about-tedx-page page-container">
             <PageTitle title="TED" />
-            <Container>
             <Row className='tedXRow'>
                 <Col lg="6">
-                <p>TED is a nonprofit organization devoted to Ideas Worth Spreading. Started as a four-day conference in California 30 years ago, TED has grown to support its mission with multiple initiatives. </p>
-                <p>The two annual TED conferences invite the world's leading thinkers and doers to Vancouver, British Columbia to speak for 18 minutes or less.</p>
+                    <p>TED is a nonprofit organization devoted to Ideas Worth Spreading. Started as a four-day conference in California 30 years ago, TED has grown to support its mission with multiple initiatives. </p>
+                    <p>The two annual TED conferences invite the world's leading thinkers and doers to Vancouver, British Columbia to speak for 18 minutes or less.</p>
                 </Col>
                 <Col lg={{ span: 4, offset: 1 }}>
-                <div className="tedImgPlaceholder"></div>
+                    <div className="tedImgPlaceholder"></div>
                 </Col>
             </Row>
             <Row>
             </Row>
             <Row>
-                <Col lg="4">
+                <Col lg="auto">
                     <div className="tedXUofWImgPlaceholder"></div>
                 </Col>
                 <Col lg={{ span: 6, offset: 1 }}>
@@ -33,14 +33,13 @@ function AboutTedX(props) {
                     <p>TEDxUofW is established to bring inspirational and informative TED style talks to the University of Washington. Since 2012, our organization has sought to give amazing speakers a receptive audience to share their passions with. </p>
                     <p>Its annual conference is made possible by a team of creative and hardworking undergraduate students. Our all student-run organization has put on a sold-out event for four years in a row, gathering a collection of great creative thinkers, scientific minds, community leaders, and much more.</p>
                 </Col>
-                
+
             </Row>
             <Row>
                 <Col lg="6">
-                <img className="tedRedLogo" src={TedRedLogo} alt="tedx logo"></img> 
+                    <img className="tedRedLogo" src={TedRedLogo} alt="tedx logo"></img>
                 </Col>
             </Row>
-            </Container>       
         </div>
     );
 }
