5afda5345d5cb6a3b6283a875e7d895683b22615
michellewu <mwu1@uw.edu>
2023-03-09 19:18:00 -0800

    add speakers page

---
+++ tedx-website/data/speakers_data.js
@@ -0,0 +1,25 @@
+import dummySpeaker from "../assets/speakerPhotos/dummySpeaker.png"
+
+const data =
+[
+    {
+        img: dummySpeaker,
+        name: "Anind K. Dey",
+        position: "Co-Founder, Co-CEO",
+        description: "Aramis O. Hamer is a visual artist and muralist in Seattle, WA. Her large-scale, acrylic paintings are inspired by the cosmos, music, nature, divine femininity, and the complexities in Black culture. Aramis has exhibited her colorful creations throughout Washington state. She also painted the “V” in Seattle’s Black Lives Matter mural. This self taught, Neddy Award winning-artist lets her imagination be her guide."
+    },
+    {
+        img: dummySpeaker,
+        name: "Anind K. Dey",
+        position: "Co-Founder, Co-CEO",
+        description: "Aramis O. Hamer is a visual artist and muralist in Seattle, WA. Her large-scale, acrylic paintings are inspired by the cosmos, music, nature, divine femininity, and the complexities in Black culture. Aramis has exhibited her colorful creations throughout Washington state. She also painted the “V” in Seattle’s Black Lives Matter mural. This self taught, Neddy Award winning-artist lets her imagination be her guide."
+    },
+    {
+        img: dummySpeaker,
+        name: "Anind K. Dey",
+        position: "Co-Founder, Co-CEO",
+        description: "Aramis O. Hamer is a visual artist and muralist in Seattle, WA. Her large-scale, acrylic paintings are inspired by the cosmos, music, nature, divine femininity, and the complexities in Black culture. Aramis has exhibited her colorful creations throughout Washington state. She also painted the “V” in Seattle’s Black Lives Matter mural. This self taught, Neddy Award winning-artist lets her imagination be her guide."
+    }
+]
+
+export default data;
