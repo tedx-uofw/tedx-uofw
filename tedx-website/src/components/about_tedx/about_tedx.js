f5075c29c5844ee3d09ca66277fbf7927dc57adb
ruhee uw <ruheer@uw.edu>
2023-03-20 17:14:04 -0400

    Added images

--- tedx-website/src/components/about_tedx/about_tedx.js
+++ tedx-website/src/components/about_tedx/about_tedx.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './about_tedx.css';
 import PageTitle from "../page_title/page_title"
 import TedRedLogo from "../../assets/redLogo.png"
-import {Row,Col} from 'react-bootstrap';
+import TeamPhoto from "../../assets/teamPhoto.png"
+
+import {Row,Col,Image} from 'react-bootstrap';
+
+import TedOfficialLogo from "../../assets/officialTedLogo.png"
 function AboutTedX(props) {
     return (
         <div className="about-tedx-page page-container">
@@ -13,14 +17,14 @@ function AboutTedX(props) {
                     <p>The two annual TED conferences invite the world's leading thinkers and doers to Vancouver, British Columbia to speak for 18 minutes or less.</p>
                 </Col>
                 <Col lg={{ span: 4, offset: 1 }}>
-                    <div className="about-tedx-tedImgPlaceholder"></div>
+                <Image className="about-tedx-ted-official-logo img-fluid" src={TedOfficialLogo} alt="official ted logo"></Image>
                 </Col>
             </Row>
             <Row className="about-tedx-tedXUofWInfo">
-                <Col lg="4">
-                    <div className="about-tedx-tedXUofWImgPlaceholder"></div>
+                <Col lg="6">
+                <Image className="about-tedx-team-photo img-fluid" src={TeamPhoto} alt="group photo"></Image>
                 </Col>
-                <Col lg={{ span: 6, offset: 2 }} >
+                <Col lg={{ span: 4, offset: 2 }} >
                     <h3 className='about-tedx-tedXUofWHeader'>TEDxUofW</h3>
                     <p>TEDxUofW is established to bring inspirational and informative TED style talks to the University of Washington. Since 2012, our organization has sought to give amazing speakers a receptive audience to share their passions with. </p>
                     <p>Its annual conference is made possible by a team of creative and hardworking undergraduate students. Our all student-run organization has put on a sold-out event for four years in a row, gathering a collection of great creative thinkers, scientific minds, community leaders, and much more.</p>
