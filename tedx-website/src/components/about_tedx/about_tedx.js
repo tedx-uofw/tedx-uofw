ada7bb47e4ad4cda23a9af9251389414064e8f6e
alicez <jyz2003@cs.washington.edu>
2023-03-20 23:30:28 -0700

    Alice edits

--- tedx-website/src/components/about_tedx/about_tedx.js
+++ tedx-website/src/components/about_tedx/about_tedx.js
@@ -13,19 +13,19 @@ function AboutTedX(props) {
         <div className="about-tedx-page page-container">
             <PageTitle title="TED" />
             <Row className='about-tedx-tedXRow'>
-                <Col lg="6">
+                <Col>
                     <p>TED is a nonprofit organization devoted to Ideas Worth Spreading. Started as a four-day conference in California 30 years ago, TED has grown to support its mission with multiple initiatives. </p>
                     <p>The two annual TED conferences invite the world's leading thinkers and doers to Vancouver, British Columbia to speak for 18 minutes or less.</p>
                 </Col>
-                <Col lg={{ span: 4, offset: 1 }}>
+                <Col lg={{ span: 4, offset: 1 }} className="about-tedx-tedx-official-logo-container">
                     <Image className='about-tedx-tedx-official-logo img-fluid' src={TedOfficialLogo} alt="TEDx official logo"></Image>
                 </Col>
             </Row>
-            <Row className="about-tedx-tedXUofWInfo align-items-center">
-                <Col lg ="4">
-                <Image className='about-tedx-team-photo img-fluid' src={TeamPhoto} alt="TEDx U of W team"></Image>
+            <Row className="about-tedx-tedXUofWInfo">
+                <Col>
+                    <Image className='about-tedx-team-photo img-fluid' src={TeamPhoto} alt="TEDx U of W team"></Image>
                 </Col>
-                <Col lg={{ span: 6, offset: 1 }} >
+                <Col lg={{ span: 5, offset: 1 }}>
                     <h1 className='about-tedx-tedXUofWHeader'>TEDxUofW</h1>
                     <p>TEDxUofW is established to bring inspirational and informative TED style talks to the University of Washington. Since 2012, our organization has sought to give amazing speakers a receptive audience to share their passions with. </p>
                     <p>Its annual conference is made possible by a team of creative and hardworking undergraduate students. Our all student-run organization has put on a sold-out event for four years in a row, gathering a collection of great creative thinkers, scientific minds, community leaders, and much more.</p>
@@ -33,7 +33,7 @@ function AboutTedX(props) {
             </Row>
             <Row>
                 <Col lg="8">
-                    <img className="about-tedx-ted-red-logo" src={TedUWRedLogo} alt="tedx logo"></img>
+                    <img className="about-tedx-tedxuofw-red-logo" src={TedUWRedLogo} alt="tedx logo"></img>
                 </Col>
             </Row>
         </div>
