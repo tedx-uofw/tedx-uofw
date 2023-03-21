a371b1879702960eff4e8c4f850af2fe820dc6e7
alicez <jyz2003@cs.washington.edu>
2023-03-21 00:45:23 -0700

    Add ASUW logo

--- tedx-website/src/data/our_sponsors_data.js
+++ tedx-website/src/data/our_sponsors_data.js
@@ -2,6 +2,7 @@ import UWAlumAssociation from "../assets/sponsor-photos/uw-alum-association.jpeg
 import HUB from "../assets/sponsor-photos/hub.png"
 import SocietyApp from "../assets/sponsor-photos/society-app.png"
 import Standard from "../assets/sponsor-photos/standard.png"
+import ASUW from "../assets/sponsor-photos/asuw.png"
 
 const data =
 [
@@ -21,5 +22,9 @@ const data =
         altText: "Standard logo",
         img: Standard
     },
+    {
+        altText: "Associated Student of the University of Washington logo",
+        img: ASUW
+    }
 ]
 export default data;
