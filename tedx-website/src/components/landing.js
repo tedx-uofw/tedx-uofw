b4265ab67cd7c5c093dbf9b25592181ff11899b8
alicez <jyz2003@cs.washington.edu>
2023-04-17 16:11:31 -0700

    Add schedule to landing page

--- tedx-website/src/components/landing.js
+++ tedx-website/src/components/landing.js
@@ -38,7 +38,8 @@ function Landing(props){
                     <h1>Seeing the Invisible</h1>
                 </div>
                 <div className="col-xs-6 col-sm-6 col-lg-6 col-md-6">
-                    <p className = "landing-theme-content">The theme “Seeing the Invisible” places the
+                    <p>
+                        The theme “Seeing the Invisible” places the
                         spotlight on ideas, experiences, and passions that
                         are not typically noticed in our daily lives or
                         recognized by the majority of society.
@@ -47,8 +48,41 @@ function Landing(props){
                         conference around this theme, we hope to
                         illuminate some of the beautiful accomplishments
                         and perspectives our community and the world may
-                        not have realized before.</p>
+                        not have realized before.
+                    </p>
                 </div>            
+            </div>
+            <div className = "landing-schedule-container row mt-60">
+                <div className="col-xs-6 col-sm-6 col-lg-6 col-md-6">
+                    <h1>Conference Schedule</h1>
+                </div>
+                <div className = "row">
+                    <div className="col">
+                        <p id="landing-left-schedule">
+                        9:00 am: Doors open<br></br>
+                        10:10 am: How negative emotions can lead to a better life! by Nazila Sawhney<br></br>
+                        10:30 am: The Power of Belonging in Nature by Dr. Pooja Tandon<br></br>
+                        10:50 am: UW Comedy Club<br></br>
+                        11:10 am: Seeing ADHD for what it is, an epidemic by Christian Murphy<br></br>
+                        11:30 am: Raffle drawing<br></br>
+                        11:40 am: Invisible Work: The things we do that no one will ever see by Michael Huang<br></br>
+                        12:00 pm: Lunch<br></br>
+                        </p>
+                    </div>
+                    <div className="col">
+                        <p>
+                        1:00 pm: Accelerating Scientific Discovery with Machine Learning by Professor Nathan Kutz<br></br>
+                        1:20 pm: UW Traditional Chinese Dance<br></br>
+                        1:40 pm: Deconstructing Leadership by Patricia Allen<br></br>
+                        2:00 pm: Listen to Learn by Matt Crevin<br></br>
+                        2:20 pm: Raffle drawing<br></br>
+                        2:30 pm: 4 Keys to Achieving Your Dream by Dr. Susie Sharpe<br></br>
+                        2:50 pm: Speakers Q & A<br></br>
+                        3:30 pm: Networking session<br></br>
+                        4:00 pm: Conference concludes<br></br>
+                        </p>
+                    </div>         
+                </div>
             </div>
             <div className = "landing-venue-container row mt-60">
                 <div className="col-xs-6 col-sm-6 col-lg-6 col-md-6">
