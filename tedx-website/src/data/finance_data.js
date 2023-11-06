fdb9200d8b6863678cc550e00023f390225d1a59
tedx-uofw <tedxuofw@uw.edu>
2023-11-05 21:30:20 -0800

    Revert "Revert "Speakers page""
    This reverts commit 68587c2815547339e6b04466e03939fad2f4c7a8.

---
+++ tedx-website/src/data/finance_data.js
@@ -0,0 +1,45 @@
+import kelleyPhoto from "../assets/teamPhotos/kelleyZImg.png"
+import clementinePhoto from "../assets/teamPhotos/clementineMImg.png"
+import timonPhoto from "../assets/teamPhotos/timonWImg.png"
+import emanuelPhoto from "../assets/teamPhotos/emanuelCImg.png"
+import serenePhoto from "../assets/teamPhotos/sereneSImg.png"
+import sunnyPhoto from "../assets/teamPhotos/sunnyKImg.png"
+
+const data =
+[
+    {
+        name: "Kelley Zeng",
+        major: "Business Major",
+        img: kelleyPhoto
+    },
+    {
+        name: "Clementine McLemore",
+        major: "Business Administration Major",
+        img: clementinePhoto
+    },
+    {
+        name: "Timon Win",
+        major: "Finance & Information Systems Major",
+        img: timonPhoto
+    },
+    {
+        name: "Emanuel Costea",
+        major: "Finance & Information Systems Major",
+        img: emanuelPhoto
+
+    },
+    {
+        name: "Serene Salam",
+        major: "Integrated Social Sciences Major",
+        img: serenePhoto
+
+    },
+    {
+        name: "Sunny Ko",
+        major: "Sociology Major",
+        img: sunnyPhoto
+
+    }
+
+]
+export default data;
---
+++ tedx-website/src/ted_routes.js
@@ -0,0 +1,24 @@
+import { Routes, Route } from 'react-router-dom'
+import AboutTeam from './components/about_team/about_team'
+import OurSponsors from './components/our_sponsors/our_sponsors';
+import Landing from "./components/landing"
+import AboutTedX from './components/about_tedx/about_tedx';
+import SpeakerList from './components/speaker_list/speakerlist';
+
+function TedRoutes() {
+    /* To add a new route, add the following inside <Routes></Routes>.
+       Make sure to change /your-path to the desired path and <MyComponent /> to your component.
+       <Route path="/your-path" element = { <MyComponent /> } /> */
+    return (
+        <Routes>
+            <Route path="/" element = { <Landing /> } />
+            <Route path="/meet-the-team" element = { <AboutTeam /> } />
+            <Route path="/sponsors" element = { <OurSponsors /> } />
+            <Route path="/about-tedx" element = { <AboutTedX /> } />
+            <Route path="/speakers" element = { <SpeakerList />} />
+        </Routes>
+    
+    );
+}
+
+export default TedRoutes;
